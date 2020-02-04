e93c9eff6c661d358d17b43ff49c13c9418ff2c3
Bennett Kedrosky <bkedrosky2022@francisparker.org>
2020-02-04 15:00:46 -0800

    still trying

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function main(){
     base_image = new Image();
     base_image.src = 'Art/Andy Warhol/Warhol1.jpg';
     base_image.onload = function(){
-      context.drawImage(base_image, 0, 0);
+      context.drawImage(base_image, 0, 0, 100, 100);
   }
 }
 
