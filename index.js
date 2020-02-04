972cb2ac5ff8f2416db08f45bc41decd78b927c8
Bennett Kedrosky <bkedrosky2022@francisparker.org>
2020-02-04 14:51:47 -0800

    added image

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function main(){
   var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var context = canvas.getContext('2d');
 
-    var Warhol1 = document.getElementById("Warhol1");
-    context.drawImage(Warhol1,  10, 10, 150, 180)
+  if (canvas.getContext) {
+  var context = canvas.getContext('2d');
+
+    base_image = new Image();
+    base_image.src = 'Art/Andy Warhol/Warhol1.jpg';
+    base_image.onload = function(){
+      context.drawImage(base_image, 0, 0);
   }
 }
