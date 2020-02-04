cfd90bc37f97b82f1440baa456d9ffe0c0afc147
Bennett Kedrosky <bkedrosky2022@francisparker.org>
2020-02-04 12:57:00 -0800

    added image

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ function main(){
   if (canvas.getContext) {
     var context = canvas.getContext('2d');
 
-    context.fillRect(20,20,100,100);
-    context.clearRect(40,40,60,60);
-    context.strokeRect(45,45,50,50);
+    var Warhol1 = document.getElementById("Warhol1");
+    context.drawImage(Warhol1,  10, 10, 150, 180)
   }
 }
